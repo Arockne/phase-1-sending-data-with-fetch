a68087f2f8354acb9fa5646498814b1686365587
Daniel Calvin <arockne2003@yahoo.com>
2021-07-31 06:09:35 -0700

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add your code here
+
+function submitData(name, email) {
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      "Content-type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({name, email})
+  })
+  .then(resp => resp.json())
+  .then(json => document.body.textContent += json.id + '\n')
+  .catch(error => document.body.textContent += error.message + '\n');
+}
